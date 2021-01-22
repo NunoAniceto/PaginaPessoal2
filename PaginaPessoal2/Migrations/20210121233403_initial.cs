c6bbd7e598ad4104ea9c3cc3e35b6573f49e4658
Nuno Aniceto <anicetonuno74@gmail.com>
2021-01-22 02:01:00 +0000

    modificado index e criar experiencias

---
+++ PaginaPessoal2/Migrations/20210121233403_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaginaPessoal2.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Experiencia",
+                columns: table => new
+                {
+                    ExperienciaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ano = table.Column<string>(nullable: true),
+                    Empresa = table.Column<string>(nullable: true),
+                    Cargo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Experiencia", x => x.ExperienciaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Experiencia");
+        }
+    }
+}
--- PaginaPessoal2/Models/Experiencia.cs
+++ PaginaPessoal2/Models/Experiencia.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace PaginaPessoal2.Models
     public class Experiencia
     {
             public int ExperienciaId { get; set; }
+
+            [Required(ErrorMessage = "Deve preencher o(s) ano(s).")]
             public string Ano { get; set; }
+
+            [Required(ErrorMessage = "Deve preencher a empresa.")]
             public string Empresa { get; set; }
+
+            [Required(ErrorMessage = "Deve preencher o cargo.")]
             public string Cargo { get; set; }
             
         
